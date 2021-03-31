84b034d705ea786b2e0ecb77338d40c44edef560
Tar Iryna <tar86irina@gmail.com>
2021-03-31 16:25:15 +0300

    goit-markup-hw-08

--- sass/js/modal.js
+++ sass/js/modal.js
@@ -1,14 +1,14 @@
 <script>
-const refs = {
-    openModalBtn: document.querySelector("[data-open-modal]"),
-    closeModalBtn: document.querySelector("[data-close-modal]"),
-    backdrop: document.querySelector("[data-backdrop]"),
-};
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
-refs.backdrop.addEventListener("click", logBackdropClick);
-
-function toggleModal() {
-    refs.backdrop.classList.toggle("is-hidden");
-}
-</script>
+        const refs = {
+            openModalBtn: document.querySelector("[data-open-modal]"),
+            closeModalBtn: document.querySelector("[data-close-modal]"),
+            backdrop: document.querySelector("[data-backdrop]"),
+        };
+        refs.openModalBtn.addEventListener("click", toggleModal);
+        refs.closeModalBtn.addEventListener("click", toggleModal);
+        refs.backdrop.addEventListener("click", logBackdropClick);
+        
+        function toggleModal() {
+            refs.backdrop.classList.toggle("is-hidden");
+        }
+        </script>
